66e0b96c306aabdaa11ac0de4e6dadc48cffafed
AKLIGO <koffitsejeromea@gmail.com>
2026-03-25 17:49:45 +0000

    les liens actifs dans Next_Js

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+import Link from "next/link"
+
 export default function Home() {
-  return <h2>
-    welcome to nextjs 13 app router
-  </h2>
+  return (
+    <>
+      <h2>
+           welcome to nextjs 13 app router
+     </h2>
+     <Link href="/blog">Go to Blog  </Link>
+     <br />
+     <Link href="/products">Go to Products  </Link>
+
+    </>
+  )
 }
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,11 +1,20 @@
+import Link from "next/link"
+
 export default function ProductsList(){
-    return <><h2>
+
+    const productId=100;
+    return <>
+    <Link href="/">Go to Home  </Link>
+    <br />
+    
+    <h2>
         welcome to products list page
     </h2>
     <div>
         <h1>Products List</h1>
-        <h3>product 1</h3>
-        <h3>product 2</h3>
-        <h3>product 3</h3>
+        <h3><Link href="/products/1">Product 1</Link></h3>
+        <h3><Link href="/products/2">Product 2</Link></h3>
+        <h3><Link href="/products/3">Product 3</Link></h3>
+        <h3><Link href={`/products/${productId}`}>Product {productId}</Link></h3>
     </div></> 
 }
